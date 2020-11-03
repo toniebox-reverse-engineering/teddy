49d47ddbe3098b23a34cd96174297f7d8fb1e42d
g3gg0 <geggo@g3gg0.de>
2020-11-03 20:45:00 +0100

    allow enter in message text

--- TeddyBench/ReportForm.Designer.cs
+++ TeddyBench/ReportForm.Designer.cs
@@ -94,7 +94,6 @@
             // 
             // ReportForm
             // 
-            this.AcceptButton = this.btnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
